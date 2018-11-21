365f30e24621528cb8a88dcc0c1823f8df1b32d4
Anna Giża <ania.giza@tlen.pl>
2018-11-22 00:30:39 +0100

    clean up

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,10 @@
-var Movie = function(title, rate, description, id){
+var Movie = function(title, rate, description, id){//konstruktor tworzocy obiekty typu film
 	this.title = title;
 	this.rate = rate;
 	this.description = description;
 	this.id = id;
 
-	this.createLiElement = function(){
+	this.createLiElement = function(){//zwraca element listy
 		return `
 		<li id="${this.id}">
 			Tytul: ${this.title} --- Ocena: ${this.rate} <br/> Opis: ${this.description}
@@ -14,7 +14,7 @@ var Movie = function(title, rate, description, id){
 }
 
 
-var Server = function(){
+var Server = function(){//tworze server
 	this.host = 'localhost';
 	this.port = '3000';
 
@@ -24,16 +24,16 @@ var Server = function(){
 }
 
 
-var MoviesHelper = function(){
+var MoviesHelper = function(){//dodawnie usuwanie filmow
 	this.server = new Server();
 	this.endpoint = 'movies';
 
 	this.getMovies = function(){
-		$('#read ul').empty();
-		$.ajax({
-		  url: this.server.getUrl(this.endpoint),
+		$('#read ul').empty();//czysci strone
+		$.ajax({//wykonuje ajax
+		  url: this.server.getUrl(this.endpoint),//endpoint na movies
 		})
-		.done(function(resp) {
+		.done(function(resp) {//to co zwraca server to tablica dlatego for do stworzenie nowvego obiektu movie
 			for(var i = 0; i < resp.length; i++){
 				var movie = new Movie(
 					resp[i]['title'],
@@ -43,7 +43,7 @@ var MoviesHelper = function(){
 				);
 				console.log(movie);
 
-				$('#read ul').append(movie.createLiElement());
+				$('#read ul').append(movie.createLiElement());//lista li ddaje i zwraca listę
 			}
 		});
 	};
@@ -59,18 +59,18 @@ var MoviesHelper = function(){
 		  }
 		})
 		.done(function(resp) {
-			movie.id = resp['id'];
-			$('#read ul').append(movie.createLiElement());
+			movie.id = resp['id'];//aktualizuje movie id
+			$('#read ul').append(movie.createLiElement());//dodalismy id kt przyslal server
 		});
 	};
 
 	this.deleteMovie = function(id){
 		$.ajax({
-		  url: this.server.getUrl(this.endpoint) + id + '/',
-		  method: 'DELETE'
+		  url: this.server.getUrl(this.endpoint) + id + '/',//options leci z automatu
+		  method: 'DELETE'//zmieniam metode na delete
 		})
 		.done(function(resp) {
-			$('#' + id).remove();
+			$('#' + id).remove();//usowam element z listy
 		});
 	}
 }
@@ -79,12 +79,12 @@ var MoviesHelper = function(){
 $(document).ready(function(){
 	var moviesHelper = new MoviesHelper();
 
-	moviesHelper.getMovies();
+	moviesHelper.getMovies();//po wejsciu na strone wywołuje sie
 	
 
 	$(document).on('click', 'form input[type="submit"]', function(e){
-		e.preventDefault();
-		var movie = new Movie(
+		e.preventDefault();//żeby się nie przeładowało
+		var movie = new Movie(//tworzenie obiektu movie
 			$('#title').val(),
 			$('#rate').val(),
 			$('#description').val()
